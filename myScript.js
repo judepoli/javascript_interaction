49e3b6d4d686bfe1ee1c33d654dfb7da54e3e796
Jude <113306646+judepoli@users.noreply.github.com>
2023-11-25 15:24:12 -0800

    final

--- myScript.js
+++ myScript.js
@@ -12,6 +12,7 @@ fortuneButton.addEventListener("click", function(){
 	generate(currentInputText);
 	restyleSize();
 	restyleColor();
+	restyleDirection();
 });
 
 halfButton.addEventListener("click", function(){
@@ -25,11 +26,11 @@ function generate(incUserInput){
 	console.log(randomIndex);
 	let selectedRandomFortunesText = fortunes[randomIndex];
 	console.log(selectedRandomFortunesText);
-	fortuneOutput.innerText = incUserInput + ": " + selectedRandomFortunesText;
+	fortuneOutput.innerText = "My dearest " + incUserInput + ", " + selectedRandomFortunesText;
 }
 
 function excitingMath(incomingNumber) {
-	let mathResult = (incomingNumber / 2.0) * 10000;
+	let mathResult = (incomingNumber / 2.0);
 	alert(mathResult);
 }
 
@@ -41,14 +42,21 @@ function restyleSize(){
 
 }
 
+
+
 function restyleColor(){
-	let fontColor = ["#521250", "#1c271", "#FF0000"];
+	let fontColor = ["#521250", "#1c271", "#FF0000", "#FFFFFF", "#000000", "#458967", "#902376", "#788769"];
 	let randomIndex = Math.floor(Math.random() * fontColor.length);
-	console.log(randomIndex);
-	fortuneOutput.style.fontColor = fontColor[randomIndex]; 
+	fortuneOutput.style.color = fontColor[randomIndex]; 
 
 }
 
+
+function restyleDirection (){
+	let fontDirection = ["ltr" , "rtl", "initial", "inherit"];
+	let randomIndex = Math.floor(Math.random() * fontDirection.length);
+	fortuneOutput.style.direction = fontDirection[randomIndex];
+}
 
 
 
